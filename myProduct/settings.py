162160f9d9dcde0896187fdfa82ba62b89209606
mbanaso chukwudi <chuksmbanasoj@gmail.com>
2021-11-21 04:11:07 -0600

    Updated settings

--- myProduct/settings.py
+++ myProduct/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import django_heroku
+from pathlib import Path
 import os
 import cloudinary_storage
 import cloudinary
