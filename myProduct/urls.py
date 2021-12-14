79fa49d4141a002a3c7fb2761ac92acb60a86e55
D-coder <chuksmbanasoj@gmail.com>
2021-12-14 02:47:11 +0100

    Updated Settings

--- myProduct/urls.py
+++ myProduct/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views
 
+admin.site.site_header = 'Product Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Product.urls')),
