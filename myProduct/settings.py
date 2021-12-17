73ab73d652222b38f5cc267dfc9398faea69ff9c
mbanaso chukwudi <chuksmbanasoj@gmail.com>
2021-12-17 17:00:20 +0100

    Updated files

--- myProduct/settings.py
+++ myProduct/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
