fbac69d06fa3df7fbce5f3a283f00a33a641edbe
D-coder <chuksmbanasoj@gmail.com>
2021-12-22 20:20:58 +0100

    Updated forms

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
 
 class userform(UserCreationForm):
     email = forms.EmailField()
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
 
     class Meta:
         model = User
